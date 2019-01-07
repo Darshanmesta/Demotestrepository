63c8d4180cee77e14e744594eed1c2bed1da8fae
GPG <GPG@MIDAS-SANDY-GPG.MIDASNET.LOCAL>
2019-01-07 08:16:15 -0700

    modified changes

---
+++ MavenDemoTest/src/test/java/com/qa/testing/Method3.java
@@ -0,0 +1,48 @@
+package com.qa.testing;
+
+import org.testng.Assert;
+import org.testng.Reporter;
+import org.testng.annotations.Test;
+
+public class Method3 {
+	
+	public class Method1 {
+		
+		@Test(priority=1)
+		public void addM()
+		{
+			Reporter.log("Addition", true); 
+			int a=10;
+			int b=20;
+			Assert.assertEquals(30, a+b);
+		}
+		
+		@Test(priority=0)
+		public void subM()
+		{
+			Reporter.log("Subtration", true);
+			int a=10;
+			int b=20;
+			Assert.assertEquals(10, b-a);
+		}   
+		@Test(priority=-2)
+		public void mulM()
+		{
+			Reporter.log("Multiplication", true);
+			int a=10;
+			int b=20;
+			Assert.assertEquals(200, a*b);
+		}
+		@Test(priority=-3)
+		public void divM()
+		{
+			Reporter.log("Division", true);
+			int a=10;
+			int b=100;
+			Assert.assertEquals(10, b/a);
+		}
+
+	}
+
+
+}
